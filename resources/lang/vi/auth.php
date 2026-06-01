1d0d4fa098970336a54a20ce5f9ac67fcce78256
Nguyen Thanh Tung <tung19022003@gmail.com>
2026-06-01 16:17:32 +0700

    fix

--- resources/lang/vi/auth.php
+++ resources/lang/vi/auth.php
@@ -4,5 +4,5 @@ return [
     'failed' => 'Thông tin đăng nhập không chính xác.',
     'failed_with_attempts' => 'Tài khoản hoặc mật khẩu không chính xác. Bạn còn :attempts lần thử.',
     'password' => 'Mật khẩu không chính xác.',
-    'throttle' => 'ạn đã đăng nhập sai quá số lần cho phép. Vui lòng thử lại sau :seconds giây.',
+    'throttle' => 'Bạn đã đăng nhập sai quá số lần cho phép. Vui lòng thử lại sau :seconds giây.',
 ];
